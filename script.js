586ab61f1ef5e11134f33bd070f1cfddce24791e
jumanaaaaa <2111305@iub.edu.bd>
2024-04-26 11:01:20 +0600

    com3

--- script.js
+++ script.js
@@ -8,3 +8,45 @@ const mealResults = document.getElementById('meal-results');
 const showAllBtn = document.getElementById('show-all-btn');
 
 searchBtn.addEventListener('click', searchMeals);
+
+function searchMeals() {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm !== '') {
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          mealResults.innerHTML = '';
+          if (data.meals) {
+            const meals = data.meals.slice(0, 5);
+            meals.forEach(meal => {
+              const mealItem = createMealItem(meal);
+              mealResults.appendChild(mealItem);
+            });
+            if (data.meals.length > 5) {
+              showAllBtn.style.display = 'block';
+              showAllBtn.onclick = () => {
+                data.meals.slice(5).forEach(meal => {
+                  const mealItem = createMealItem(meal);
+                  mealResults.appendChild(mealItem);
+                });
+                showAllBtn.style.display = 'none';
+              };
+            } 
+            else {
+              showAllBtn.style.display = 'none';
+            }
+          } 
+          else {
+            mealResults.innerHTML = '<p>No meals found.</p>';
+            showAllBtn.style.display = 'none';
+          }
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+    }
+  }
+
+
+

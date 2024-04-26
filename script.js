f3a922a54a487143772a1a122a9a9bc9c309ebbb
jumanaaaaa <2111305@iub.edu.bd>
2024-04-26 10:50:12 +0600

    commit2

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+
+
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const mealResults = document.getElementById('meal-results');
+const showAllBtn = document.getElementById('show-all-btn');
+
+searchBtn.addEventListener('click', searchMeals);

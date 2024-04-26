1b6c68f5a323bef93393c0071f24338bf8a5bae9
jumanaaaaa <2111305@iub.edu.bd>
2024-04-26 11:17:57 +0600

    commit4

--- script.js
+++ script.js
@@ -16,28 +16,41 @@ function searchMeals() {
       fetch(url)
         .then(response => response.json())
         .then(data => {
+
+
+
           mealResults.innerHTML = '';
+
+
+
           if (data.meals) {
             const meals = data.meals.slice(0, 5);
             meals.forEach(meal => {
               const mealItem = createMealItem(meal);
               mealResults.appendChild(mealItem);
             });
+
+
             if (data.meals.length > 5) {
               showAllBtn.style.display = 'block';
               showAllBtn.onclick = () => {
+
                 data.meals.slice(5).forEach(meal => {
                   const mealItem = createMealItem(meal);
                   mealResults.appendChild(mealItem);
                 });
+
+
                 showAllBtn.style.display = 'none';
               };
-            } 
-            else {
+
+
+            } else {
               showAllBtn.style.display = 'none';
             }
-          } 
-          else {
+
+
+          } else {
             mealResults.innerHTML = '<p>No meals found.</p>';
             showAllBtn.style.display = 'none';
           }
@@ -47,6 +60,26 @@ function searchMeals() {
         });
     }
   }
+
+ 
+function createMealItem(meal) {
+    
+    const mealItem = document.createElement('div');
+    mealItem.classList.add('meal-item');
+
+
+
+    mealItem.innerHTML = `
+      <h2>Meal ID: ${meal.idMeal}</h2>
+      <h3>Meal Name: ${meal.strMeal}</h3>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+      <p>Cooking Instructions:</p>
+      <p>${meal.strInstructions}</p>
+    `;
+    return mealItem;
+  }
+  
+
 
 
 
